0ccfc38c9552f5f42b255a1fd54a6da13f0acdc0
Anh Truong Lap Trinh <119102920+truongdolaptrinh@users.noreply.github.com>
2023-07-24 22:57:24 +0700

    pagination

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,38 +1,35 @@
-<?php 
+<?php
 
 session_start();
 
+if (isset($_SESSION['cart'])) {
+    $cart_id = array_column($_SESSION['cart'], "id");
 
+    if (!in_array($_POST['id'], $cart_id)) {
+        $item_array = array(
+            "id" => $_POST['id'],
+            "name" => $_POST['name'],
+            "price" => $_POST['price'],
+            "quantity" => $_POST['quantity']
+        );
 
-  if (isset($_SESSION['cart'])) {
+        
+        $_SESSION['cart'][] = $item_array;
+        var_dump($item_array);
+die();
+    } else {
+        // If the product already exists in the cart, update the quantity
+        $index = array_search($_POST['id'], $cart_id);
+        $_SESSION['cart'][$index]['quantity'] += $_POST['quantity'];
+    }
+} else {
+    $item_array = array(
+        "id" => $_POST['id'],
+        "name" => $_POST['name'],
+        "price" => $_POST['price'],
+        "quantity" => $_POST['quantity']
+    );
 
-  	$cart_id = array_column($_SESSION['cart'], "id");
-
-  	if (!in_array($_POST['id'], $_SESSION['cart'])) {
-  		$item_array = array(
-         "id" => $_POST['id'],
-         "name" => $_POST['name'],
-         "price" => $_POST['price'],
-         "quantity" => $_POST['quantity']
-  	);
-
-
-  $_SESSION['cart'][] = $item_array;
-  	}
-  	
-  }else{
-
-  	$item_array = array(
-         "id" => $_POST['id'],
-         "name" => $_POST['name'],
-         "price" => $_POST['price'],
-         "quantity" => $_POST['quantity']
-  	);
-
-
-  $_SESSION['cart'][] = $item_array;
-  }
-
-
-
- ?>
+    $_SESSION['cart'][] = $item_array;
+}
+?>
